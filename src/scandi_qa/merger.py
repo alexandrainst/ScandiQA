4b2421458269d58ea172f8062a6bd4b1dc7bf049
saattrupdan <dan.nielsen@alexandra.dk>
2022-06-16 16:55:16 +0200

    fix: Remove temporary truncation of NQ

--- src/scandi_qa/merger.py
+++ src/scandi_qa/merger.py
@@ -41,9 +41,6 @@ class Merger:
         )
         self.embedder = Embedder()
 
-        # TEMP
-        self.nq = self.nq.select(range(100))
-
     def build_mkqa(self) -> pd.DataFrame:
         """Builds the MKQA dataset for the given language.
 
