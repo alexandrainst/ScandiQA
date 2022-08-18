4cc757a4cb267d70bdf941d16543eef3d80baa25
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2022-08-18 11:28:11 +0200

    feat: Import builder in __init__

--- src/scandi_qa/__init__.py
+++ src/scandi_qa/__init__.py
@@ -4,5 +4,7 @@
 
 import pkg_resources
 
+from .builder import QADatasetBuilder
+
 # Fetches the version of the package as defined in pyproject.toml
 __version__ = pkg_resources.get_distribution("scandi_qa").version
